5e8280e2df4f99d3e0bd4c80da88c585e8bd07ca
SergeyBel <belovseregas@gmail.com>
2020-09-13 13:45:32 +0300

    add ffieldpolynom examples

---
+++ examples/ffieldpolynom.py
@@ -0,0 +1,27 @@
+from cryptofield.fieldpolynom import *
+
+F = FField(4)
+x = FPolynom(F, [1, 0, 5, 15])
+print (x)
+y = FPolynom(F, [0, 7, 8, 3])
+print (y)
+print ('Arithmetic:')
+print (x + y)
+print (x - y)
+print (x * y)
+print (x / y)
+print (x % y)
+
+
+z = x + y
+print ()
+print ('Polynom:', z)
+print ('Deg:', z.deg())
+print ('Rank:', z.rank())
+print ('Derivative:', z.derivative())
+print ('Values for all x:', z.values())
+print ('Is linear:', z.isLinear())
+print ('Is affine:', z.isAffine())
+
+
+
