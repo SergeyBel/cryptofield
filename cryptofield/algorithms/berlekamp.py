4af2509edfbbaab0afe1d1c38e933d695ab0d5f4
SergeyBel <belovseregas@gmail.com>
2020-09-19 20:41:25 +0300

    space instead tabs

--- cryptofield/algorithms/berlekamp.py
+++ cryptofield/algorithms/berlekamp.py
@@ -4,127 +4,127 @@ from cryptofield.algorithms.field_algorithms import *
 
 # functions for compatibility with GenericMatrix constructor
 def AddAbstract(x, y):
-	return x + y
-	
+  return x + y
+  
 def SubAbstract(x, y):
-	return x - y
-	
+  return x - y
+  
 def MulAbstract(x, y):
-	return x * y
-	
+  return x * y
+  
 def DivAbstract(x, y):
-	return x / y
+  return x / y
 
 def EqAbstract(x, y):
-	return x == y	
-	
-			
-	
+  return x == y 
+  
+      
+  
 
-# Expend row 0's for length n	
+# Expend row 0's for length n 
 def ExpendRow(F, row, n):
-	m = n - len(row)
-	for i in range(m):
-		row.append(FElement(F, 0))
-	return row
-		
-	
-# Get set of GCD(f, h-c) where c in Field		
+  m = n - len(row)
+  for i in range(m):
+    row.append(FElement(F, 0))
+  return row
+    
+  
+# Get set of GCD(f, h-c) where c in Field   
 def GCDSet(F, f, h):
-	gcds = list()
-	N = 2**F.n
-	for i in range(N):
-		c = FPolynom(F, [i])
-		g = PolynomEquilid(F, f, h - c)
-		if g.deg() > 0:
-			gcds.append(g)
-	return gcds
+  gcds = list()
+  N = 2**F.n
+  for i in range(N):
+    c = FPolynom(F, [i])
+    g = PolynomEquilid(F, f, h - c)
+    if g.deg() > 0:
+      gcds.append(g)
+  return gcds
 
-	
+  
 def Root(F, a):
-	#2-root of a, x^2=a => x = a^(N/2)
-	N = 2**F.n
-	return FPow(F, a, int(N / 2))
-	
+  #2-root of a, x^2=a => x = a^(N/2)
+  N = 2**F.n
+  return FPow(F, a, int(N / 2))
+  
 
 def PolynomPRoot(F, f):
-	for i in range(len(f.c)):
-		if not f.c[i] == FElement(F, 0):
-			t = Root(F, f.c[i])
-			f.c[i] = FElement(F, 0)
-			f.c[int (i / 2)] = t
-	f.correct()
-	return f
-	
-	
-	
+  for i in range(len(f.c)):
+    if not f.c[i] == FElement(F, 0):
+      t = Root(F, f.c[i])
+      f.c[i] = FElement(F, 0)
+      f.c[int (i / 2)] = t
+  f.correct()
+  return f
+  
+  
+  
 def Berlekamp(F, polynom):
-	f = polynom.copy()
-	factors = list()
-	if f.deg() <= 1:
-		factors.append(f)
-		return factors
+  f = polynom.copy()
+  factors = list()
+  if f.deg() <= 1:
+    factors.append(f)
+    return factors
 
-	if not f.c[-1] == FElement(F, 1):
-		factors.append(FPolynom(F, [f.c[-1]], True))
-		f.normalize()
-	d = f.derivative()
-	gcd = PolynomEquilid(F, f, d)
-	if gcd == FPolynom(F, [1]):
-		decomp = Berl(F, f)
-		factors = factors + list(decomp)
-	elif gcd == f:
-		# f = g(x)^p
-		# it mean it nedd to find h(x) such f(x) = h(x)^(p^s)
-		# then apply to h(x)
-		k = 1
-		while gcd == f:
-			f = PolynomPRoot(F, f)
-			k = k * 2
-			d = f.derivative()
-			gcd = PolynomEquilid(F, f, d)
-		decomp = Berl(F, f)
-		factors = factors + list(decomp) * k
-	else:
-		r = f / gcd
-		factors1 = Berlekamp(F, gcd)
-		factors2 = list(Berl(F, r))
-		factors = factors + factors1 + factors2
-					
-	return factors
-		
-	
+  if not f.c[-1] == FElement(F, 1):
+    factors.append(FPolynom(F, [f.c[-1]], True))
+    f.normalize()
+  d = f.derivative()
+  gcd = PolynomEquilid(F, f, d)
+  if gcd == FPolynom(F, [1]):
+    decomp = Berl(F, f)
+    factors = factors + list(decomp)
+  elif gcd == f:
+    # f = g(x)^p
+    # it mean it nedd to find h(x) such f(x) = h(x)^(p^s)
+    # then apply to h(x)
+    k = 1
+    while gcd == f:
+      f = PolynomPRoot(F, f)
+      k = k * 2
+      d = f.derivative()
+      gcd = PolynomEquilid(F, f, d)
+    decomp = Berl(F, f)
+    factors = factors + list(decomp) * k
+  else:
+    r = f / gcd
+    factors1 = Berlekamp(F, gcd)
+    factors2 = list(Berl(F, r))
+    factors = factors + factors1 + factors2
+          
+  return factors
+    
+  
 def Berl(F, f):
-	n = f.deg()
-	q = 2**F.n
-	I = FMatrix(F, n, n)
-	I.ident()
-	x = FPolynom(F, [0, 1])
-	
-	B = FMatrix(F, n, n)
-	for i in range(0, n):
-		y = x ** (i * q) #bi = x^(iq) in GF(q)
-		y = y % f
-		B.setRow(i, ExpendRow(F, y.c, n))
-	B = B - I
-	basis = B.kerBasis()
-	k = len(basis)
-	decomp = set()
-	decomp.add(f)
-	i = 0
-	while len(decomp) < k:
-		tempDecomp = set()
-		h = FPolynom(F, basis[i], True)
-		if h == FPolynom(F, [1]):
-			i = i + 1
-			continue
-		
-		for d in decomp:
-			g =  GCDSet(F, d, h)
-			tempDecomp = tempDecomp.union(g)
-		i = i + 1
-		decomp = tempDecomp
-	return decomp
+  n = f.deg()
+  q = 2**F.n
+  I = FMatrix(F, n, n)
+  I.ident()
+  x = FPolynom(F, [0, 1])
+  
+  B = FMatrix(F, n, n)
+  for i in range(0, n):
+    y = x ** (i * q) #bi = x^(iq) in GF(q)
+    y = y % f
+    B.setRow(i, ExpendRow(F, y.c, n))
+  B = B - I
+  basis = B.kerBasis()
+  k = len(basis)
+  decomp = set()
+  decomp.add(f)
+  i = 0
+  while len(decomp) < k:
+    tempDecomp = set()
+    h = FPolynom(F, basis[i], True)
+    if h == FPolynom(F, [1]):
+      i = i + 1
+      continue
+    
+    for d in decomp:
+      g =  GCDSet(F, d, h)
+      tempDecomp = tempDecomp.union(g)
+    i = i + 1
+    decomp = tempDecomp
+  return decomp
 
 """
 G = FField(4)
@@ -133,7 +133,7 @@ f = FPolynom(G, [11, 0, 1])
 x = Berlekamp(G, f)
 
 for t in x:
-	print t
+  print t
 """
 
 #RandomTest()
--- cryptofield/common/common.py
+++ cryptofield/common/common.py
@@ -3,80 +3,80 @@ from cryptofield.ffield import *
 
 
 def valueToBinaryStr(value, length = False):
-	s = bin(value)[2:]
-	if length != False:
-		s  = "0" * (length - len(s)) + s
-	return s
+  s = bin(value)[2:]
+  if length != False:
+    s  = "0" * (length - len(s)) + s
+  return s
 
 def binaryStrToValue(str):
-	return int(str, 2)
+  return int(str, 2)
 
 def scalarBool(vec1, vec2):
-		s = 0
-		for i in range(0, len(vec1)):
-			s = s + ( (int(vec1[i]) & int(vec2[i])))
-		return s
+    s = 0
+    for i in range(0, len(vec1)):
+      s = s + ( (int(vec1[i]) & int(vec2[i])))
+    return s
 
 def nextBoolVec(vec):
-	t = list(vec)
-	i = len(t) - 1
-	while (t[i] == "1" and i >= 0):
-		i = i - 1
-	if i < 0:
-		return False
-	t[i] = "1"
-	for j in range(i + 1, len(vec)):
-		t[j] = "0"
-	return "".join(t)  # convert list to to string
+  t = list(vec)
+  i = len(t) - 1
+  while (t[i] == "1" and i >= 0):
+    i = i - 1
+  if i < 0:
+    return False
+  t[i] = "1"
+  for j in range(i + 1, len(vec)):
+    t[j] = "0"
+  return "".join(t)  # convert list to to string
 
 def swapArr(a, i, j):
-	c = a[i]
-	a[i] = a[j]
-	a[j] = c
+  c = a[i]
+  a[i] = a[j]
+  a[j] = c
 
 def isPowerTwo(n):
-	return n > 0 and not ((n & (n - 1)))
+  return n > 0 and not ((n & (n - 1)))
 
 def numberDivisors(n):
-	divisors = []
-	for i in range(1, n + 1):
-		if n % i == 0:
-			divisors.append(i)
-	return divisors
+  divisors = []
+  for i in range(1, n + 1):
+    if n % i == 0:
+      divisors.append(i)
+  return divisors
 
 def isPrime(n):
-	if n == 1:
-		return False
-	if n == 2:
-		return True
-	for i in range(2, int(math.ceil(math.sqrt(n))) + 1):
-		if n % i == 0:
-			return False
-	return True
+  if n == 1:
+    return False
+  if n == 2:
+    return True
+  for i in range(2, int(math.ceil(math.sqrt(n))) + 1):
+    if n % i == 0:
+      return False
+  return True
 
 def mobius(n):
-	if n == 1:
-		return 1
-	k = 0
-	for i in range(2, n + 1):
-		if isPrime(i):
-			if n % i == 0:
-				if n % (i*i) == 0:
-					return 0
-				else:
-					k  = (k + 1) % 2
-	if k % 2 == 0:
-		return 1
-	else:
-		return -1
+  if n == 1:
+    return 1
+  k = 0
+  for i in range(2, n + 1):
+    if isPrime(i):
+      if n % i == 0:
+        if n % (i*i) == 0:
+          return 0
+        else:
+          k  = (k + 1) % 2
+  if k % 2 == 0:
+    return 1
+  else:
+    return -1
 
 def FPow(F, x, n):
-	p = FElement(F, 1)
-	while (n):
-		if (n & 1):
-			p *= x;
-			n -= 1
-		else:
-			x *= x;
-			n >>= 1;
-	return p
+  p = FElement(F, 1)
+  while (n):
+    if (n & 1):
+      p *= x;
+      n -= 1
+    else:
+      x *= x;
+      n >>= 1;
+  return p
